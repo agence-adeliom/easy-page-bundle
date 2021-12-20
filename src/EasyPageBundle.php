630a3efcf086fdc4fc02204260ded3bf2a605dae
Arnaud RITTI <arnaud.ritti@adeliom.com>
2021-12-20 16:31:25 +0100

    no message

--- src/EasyPageBundle.php
+++ src/EasyPageBundle.php
@@ -7,6 +7,9 @@ use Adeliom\EasyPageBundle\DependencyInjection\EasyPageExtension;
 
 class EasyPageBundle extends Bundle
 {
+    /**
+     * @return ExtensionInterface|null The container extension
+     */
     public function getContainerExtension()
     {
         return new EasyPageExtension();
