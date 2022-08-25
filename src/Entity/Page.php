b07c88918c2671155445c5940665c035f287e55d
Arnaud RITTI <arnaud.ritti@adeliom.com>
2022-08-25 10:57:17 +0200

    Fixes

--- src/Entity/Page.php
+++ src/Entity/Page.php
@@ -28,15 +28,15 @@ class Page
 
     use EntityIdTrait;
     use EntityTimestampableTrait {
-        EntityTimestampableTrait::__construct as private __TimestampableConstruct;
+        EntityTimestampableTrait::__construct as private TimestampableConstruct;
     }
     use EntityNameSlugTrait;
     use EntityThreeStateStatusTrait;
     use EntityPublishableTrait {
-        EntityPublishableTrait::__construct as private __PublishableConstruct;
+        EntityPublishableTrait::__construct as private PublishableConstruct;
     }
     use EntitySeoTrait {
-        EntitySeoTrait::__construct as private __SEOConstruct;
+        EntitySeoTrait::__construct as private SEOConstruct;
     }
 
     /**
