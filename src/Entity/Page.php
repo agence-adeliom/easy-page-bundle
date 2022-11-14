6e85a836d0239620c864f280077eea6c64762620
Jérôme Engeln <jerome.engeln@adeliom.com>
2022-11-14 12:42:31 +0100

    cs

--- src/Entity/Page.php
+++ src/Entity/Page.php
@@ -13,7 +13,6 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Event\LifecycleEventArgs;
 use Doctrine\ORM\Mapping as ORM;
-use Doctrine\ORM\PersistentCollection;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
