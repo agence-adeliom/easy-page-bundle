d3cff0a9ae9ac1bc8512779f4392294e8fb976a7
JeanDavidDaviet <jeandaviddaviet@gmail.com>
2026-03-16 20:59:39 +0100

    doctrine dbal 4.x upgrade wip

--- src/EventListener/DoctrineMappingListener.php
+++ src/EventListener/DoctrineMappingListener.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Adeliom\EasyPageBundle\EventListener;
 
 use Doctrine\Bundle\DoctrineBundle\Attribute\AsDoctrineListener;
--- tests/EventListener/DoctrineMappingListenerTest.php
+++ tests/EventListener/DoctrineMappingListenerTest.php
@@ -6,6 +6,7 @@ namespace Adeliom\EasyPageBundle\Tests\EventListener;
 
 use Adeliom\EasyPageBundle\EventListener\DoctrineMappingListener;
 use Adeliom\EasyPageBundle\Tests\Fixtures\Entity\TestPage;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Event\LoadClassMetadataEventArgs;
 use Doctrine\ORM\Mapping\ClassMetadata;
 use PHPUnit\Framework\Attributes\CoversClass;
@@ -16,21 +17,22 @@ final class DoctrineMappingListenerTest extends TestCase
 {
     public function testListenerMapsParentAndChildrenAssociations(): void
     {
-        $metadata = $this->getMockBuilder(ClassMetadata::class)
-            ->disableOriginalConstructor()
-            ->onlyMethods(['getName', 'hasAssociation', 'mapManyToOne', 'mapOneToMany'])
-            ->getMock();
+        $metadata = new ClassMetadata(TestPage::class);
+        $event = new LoadClassMetadataEventArgs($metadata, $this->createMock(EntityManagerInterface::class));
+        $listener = new DoctrineMappingListener(TestPage::class);
 
-        $metadata->method('getName')->willReturn(TestPage::class);
-        $metadata->method('hasAssociation')->willReturn(false);
-        $metadata->expects(self::once())->method('mapManyToOne')
-            ->with(self::callback(fn ($m) => $m['fieldName'] === 'parent' && $m['targetEntity'] === TestPage::class));
-        $metadata->expects(self::once())->method('mapOneToMany')
-            ->with(self::callback(fn ($m) => $m['fieldName'] === 'children' && $m['targetEntity'] === TestPage::class));
+        $listener->loadClassMetadata($event);
+        $listener->loadClassMetadata($event);
 
-        $event = $this->createMock(LoadClassMetadataEventArgs::class);
-        $event->method('getClassMetadata')->willReturn($metadata);
+        $parentMapping = $metadata->getAssociationMapping('parent');
+        $childrenMapping = $metadata->getAssociationMapping('children');
 
-        (new DoctrineMappingListener(TestPage::class))->loadClassMetadata($event);
+        self::assertCount(2, $metadata->getAssociationMappings());
+        self::assertSame(TestPage::class, $parentMapping->targetEntity);
+        self::assertSame('children', $parentMapping->inversedBy);
+        self::assertTrue($parentMapping->joinColumns[0]->nullable);
+        self::assertSame('SET NULL', $parentMapping->joinColumns[0]->onDelete);
+        self::assertSame(TestPage::class, $childrenMapping->targetEntity);
+        self::assertSame('parent', $childrenMapping->mappedBy);
     }
 }
